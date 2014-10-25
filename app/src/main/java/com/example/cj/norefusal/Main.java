d25b73d169629a081ec66fd9d601e3c6b2959550
ccoddes <chinjieh@gmail.com>
2014-10-26 03:14:31 +0800

    Main Popup

--- app/src/main/java/com/example/cj/norefusal/Main.java
+++ app/src/main/java/com/example/cj/norefusal/Main.java
@@ -2,10 +2,13 @@ package com.example.cj.norefusal;
 
 import android.app.Activity;
 import android.app.ActivityOptions;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class Main extends Activity {
@@ -22,15 +25,44 @@ public class Main extends Activity {
             @Override
             public void onClick(View view) {
 
-                Intent slideactivity = new Intent(Main.this, Description.class);
+                EditText rev = (EditText) findViewById(R.id.main_etRevelation);
+                EditText phrase = (EditText)findViewById(R.id.main_etCatchPhrase);
 
-                Bundle bndlanimation = ActivityOptions.makeCustomAnimation(getApplicationContext(), R.anim.animation1, R.anim.animation2).toBundle();
+                String revtext = rev.getText().toString();
+                String phrasetext = phrase.getText().toString();
 
-                startActivity(slideactivity, bndlanimation);
+                if (revtext.trim().length() > 0 && phrasetext.trim().length() > 0) {
 
+                    Intent slideactivity = new Intent(Main.this, Description.class);
 
+                    Bundle bndlanimation = ActivityOptions.makeCustomAnimation(getApplicationContext(), R.anim.animation1, R.anim.animation2).toBundle();
+
+                    startActivity(slideactivity, bndlanimation);
+
+                }
+                else{
+                    showPopUp();
+                }
 
             }
         });
+    }
+
+    private void showPopUp() {
+
+        AlertDialog.Builder helpBuilder = new AlertDialog.Builder(this);
+        helpBuilder.setTitle("Empty Fields");
+        helpBuilder.setMessage("Please fill in all fields");
+        helpBuilder.setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Do nothing but close the dialog
+                    }
+                });
+
+        // Remember, create doesn't show the dialog
+        AlertDialog helpDialog = helpBuilder.create();
+        helpDialog.show();
     }
 }
