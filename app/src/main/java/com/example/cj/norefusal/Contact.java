93f343aea92e8a0e71188e99cec7c091e7c61767
nigelkong <nigelkong.92@gmail.com>
2014-10-26 02:42:11 +0800

    SLIDE SUCKERS
    NK

--- app/src/main/java/com/example/cj/norefusal/Contact.java
+++ app/src/main/java/com/example/cj/norefusal/Contact.java
@@ -1,8 +1,10 @@
 package com.example.cj.norefusal;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,11 +21,13 @@ public class Contact extends Activity{
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Class ourClass = null;
 
-                //ourClass = Class.forName("com.example.cj.norefusal.Description");
-                Intent a = new Intent(Contact.this, End.class);
-                startActivity(a);
+               // Intent a = new Intent(Contact.this, End.class);
+                Intent slideactivity = new Intent(Contact.this, End.class);
+
+                Bundle bndlanimation = ActivityOptions.makeCustomAnimation(getApplicationContext(), R.anim.animation1, R.anim.animation2).toBundle();
+                startActivity(slideactivity, bndlanimation);
+                // startActivity(a);
 
 
             }
