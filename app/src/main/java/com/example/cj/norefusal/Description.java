8ad56e5604cd5766d0948c9911c88242634d7910
ccoddes <chinjieh@gmail.com>
2014-10-25 21:57:39 +0800

    Added linking for next buttons except to Extent

--- app/src/main/java/com/example/cj/norefusal/Description.java
+++ app/src/main/java/com/example/cj/norefusal/Description.java
@@ -1,7 +1,10 @@
 package com.example.cj.norefusal;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by User on 25/10/2014.
@@ -11,6 +14,21 @@ public class Description extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.description);
+
+        Button next = (Button) findViewById(R.id.button1);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                //ourClass = Class.forName("com.example.cj.norefusal.Description");
+                Intent a = new Intent(Description.this, Contact.class);
+                startActivity(a);
+
+
+            }
+        });
+
     }
 
 }
