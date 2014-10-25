e57b134d8b00455af309a8363e7859b0114a8d4a
khookhoo <jiexiongkhoo@gmail.com>
2014-10-25 19:58:31 +0800

    jx

--- app/src/main/java/com/example/cj/norefusal/Description.java
+++ app/src/main/java/com/example/cj/norefusal/Description.java
@@ -1,9 +1,16 @@
 package com.example.cj.norefusal;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by User on 25/10/2014.
  */
 public class Description extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
 }
