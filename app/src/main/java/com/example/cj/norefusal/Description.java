bf7dbb90dbba653389c037ddd414b18d601317db
khookhoo <jiexiongkhoo@gmail.com>
2014-10-25 20:35:40 +0800

    sdf

--- app/src/main/java/com/example/cj/norefusal/Description.java
+++ app/src/main/java/com/example/cj/norefusal/Description.java
@@ -10,7 +10,7 @@ public class Description extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.description);
     }
 
 }
