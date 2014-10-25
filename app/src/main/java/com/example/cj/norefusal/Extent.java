abc60911ff9076256d23bb07251767113f7a8162
ccoddes <chinjieh@gmail.com>
2014-10-26 01:57:35 +0800

    UI Changes

--- app/src/main/java/com/example/cj/norefusal/Extent.java
+++ app/src/main/java/com/example/cj/norefusal/Extent.java
@@ -3,10 +3,12 @@ package com.example.cj.norefusal;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupWindow;
 
 /**
  * Created by User on 25/10/2014.
