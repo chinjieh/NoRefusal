6c9decdb4e4acba7c1dda482f3a76aabc31ee1b2
nigelkong <nigelkong.92@gmail.com>
2014-10-26 00:33:23 +0800

    Added Past Rev Button
    NK

---
+++ app/src/main/java/com/example/cj/norefusal/Pastrevelations.java
@@ -0,0 +1,27 @@
+package com.example.cj.norefusal;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by deathsync on 26/10/2014.
+ */
+public class Pastrevelations extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.past_revelations);
+
+        Button next = (Button)findViewById(R.id.bPastRevmain);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent({Pastrevelations.this, Main.class);
+                startActivity(a);
+            }
+        })
+    }
+}
